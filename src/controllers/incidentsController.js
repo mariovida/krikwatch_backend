64a7124b37598f0f1171cdc198c9a88089c74c1f
Mario <mariovidovic97@gmail.com>
2025-01-31 15:39:58 +0100

    Create incident function updated

--- src/controllers/incidentsController.js
+++ src/controllers/incidentsController.js
@@ -105,6 +105,22 @@ const getIncidentById = async (req, res) => {
   }
 };
 
+const formatDate = (date) => {
+  if (date) {
+    const d = new Date(date);
+    return `${d.getFullYear()}-${(d.getMonth() + 1)
+      .toString()
+      .padStart(2, "0")}-${d.getDate().toString().padStart(2, "0")} ${d
+      .getHours()
+      .toString()
+      .padStart(2, "0")}:${d.getMinutes().toString().padStart(2, "0")}:${d
+      .getSeconds()
+      .toString()
+      .padStart(2, "0")}`;
+  }
+  return null;
+};
+
 // Create new incident
 const createIncident = async (req, res) => {
   const {
@@ -136,12 +152,24 @@ const createIncident = async (req, res) => {
   }
 
   const incidentKey = generateIncidentKey();
+  const formattedStartTime = formatDate(start_time);
+  const formattedEndTime = formatDate(end_time);
   try {
     const [result] = await db.query(
       `INSERT INTO incidents 
-        (title, description, note, website_id, status, incident_key, created_by, created_at) 
-        VALUES (?, ?, ?, ?, ?, ?, ?, NOW())`,
-      [title, description, note, website_id, statusCode, incidentKey, user_id]
+        (title, description, note, website_id, status, incident_key, incident_start, incident_end, created_by, created_at) 
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, NOW())`,
+      [
+        title,
+        description,
+        note,
+        website_id,
+        statusCode,
+        incidentKey,
+        formattedStartTime,
+        formattedEndTime,
+        user_id,
+      ]
     );
 
     res.status(201).json({
