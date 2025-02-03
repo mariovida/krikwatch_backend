04131f176d48f5c2c7c0a4f5bccb01d3ee78dc72
Mario <mario@krikstudio.com>
2025-02-03 08:57:49 +0100

    DB fix

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,23 @@
 const mysql = require("mysql2/promise");
 
-const db = mysql.createPool({
-  host: process.env.TEST_DATABASE_HOST,
-  user: process.env.TEST_DATABASE_USER,
-  password: process.env.TEST_DATABASE_PASSWORD,
-  database: process.env.TEST_DATABASE_DB,
-});
+let dbConfig;
+
+if (process.env.ENVIRONMENT === 'production') {
+  dbConfig = {
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_DB,
+  };
+} else {
+  dbConfig = {
+    host: process.env.TEST_DATABASE_HOST,
+    user: process.env.TEST_DATABASE_USER,
+    password: process.env.TEST_DATABASE_PASSWORD,
+    database: process.env.TEST_DATABASE_DB,
+  };
+}
+
+const db = mysql.createPool(dbConfig);
 
 module.exports = db;
