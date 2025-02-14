cae5157a0dec541c4ddb49efa33fdd382581f8cd
Mario <mariovidovic97@gmail.com>
2025-02-14 17:52:50 +0100

    Delete template route

--- src/routes/templatesRoutes.js
+++ src/routes/templatesRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const {
   getTemplates,
   createTemplate,
+  deleteTemplate,
 } = require("../controllers/templatesController");
 const { authMiddleware } = require("../middleware/authMiddleware");
 
@@ -9,5 +10,6 @@ const router = express.Router();
 
 router.get("/templates", authMiddleware, getTemplates);
 router.post("/templates", authMiddleware, createTemplate);
+router.delete("/templates/delete-template/:id", authMiddleware, deleteTemplate);
 
 module.exports = router;
